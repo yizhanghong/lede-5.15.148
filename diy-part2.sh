97f3b48208dab4b14f17b42e0cb45b3741481c6c
hztbhtbh <104251804+hztbhtbh@users.noreply.github.com>
2024-01-20 15:39:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
